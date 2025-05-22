c319e2751c09dfbcd7d6949de0a38e1f83724580
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-05-22 11:06:25 -0300

    update

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,24 +1,32 @@
 "use client";
 
+import Link from "next/link";
 import Image from "next/image";
 import styles from "./Home.module.css";
+import { Button } from "antd";
 
 export default function Home() {
     return (
         <div className={styles.container}>
             <div className={styles.card}>
-                    <Image className={styles.image}
-                    src="/image/eu.png" 
-                    width={200}
-                    height={200}
-                    />
-                    <h1 className={styles.name}>Eu</h1>
+                <div className={styles.imageContainer}>
+                    <Image className={styles.image} src="/images/eu.png" alt="Foto da aluna" width={200} height={200} />
+                </div>
+                <div className={styles.conteudo}>
                     <p className={styles.description}>
+                        <p>Nome: Eu</p>
                         <p>Turma: 1TDS1</p>
                         <p>Matéria: Front-end</p>
                         <p>Atividade: Avaliação</p>
                         <p>Instrutores: Marcelo Carboni e Thiago Ferreira</p>
+                        <p>DESCRIÇÃO</p>
                     </p>
+                </div>
+        
+
+            <Link href="/animals" prefetch>
+                <Button className={styles.button} type="primary">Ir para a página de animais</Button>
+            </Link>
             </div>
         </div>
     );
