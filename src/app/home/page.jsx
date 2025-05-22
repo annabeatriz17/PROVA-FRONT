f7aee4f21348f6acd222d9a303206c760730ee88
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-05-22 09:58:13 -0300

    update - intervalo

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,10 +1,25 @@
+"use client";
+
+import Image from "next/image";
 import styles from "./Home.module.css";
 
 export default function Home() {
     return (
         <div className={styles.container}>
-            <h1>Oi</h1>
-            <p>sei lá</p>
+            <div className={styles.card}>
+                    <Image className={styles.image}
+                    src="/image/eu.png" 
+                    width={200}
+                    height={200}
+                    />
+                    <h1 className={styles.name}>Eu</h1>
+                    <p className={styles.description}>
+                        <p>Turma: 1TDS1</p>
+                        <p>Matéria: Front-end</p>
+                        <p>Atividade: Avaliação</p>
+                        <p>Instrutores: Marcelo Carboni e Thiago Ferreira</p>
+                    </p>
+            </div>
         </div>
     );
 }
